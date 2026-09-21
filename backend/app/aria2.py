10641dcf70fb665e5760a4017a468865d2a82c0c
dest16 <dest16@163.com>
2026-09-21 21:56:03 +0800

    refactor: remove bundled downloader files

--- backend/app/aria2.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible imports for integrations that used the old module."""
-
-from .transmission import TransmissionClient as Aria2Client
-from .transmission import TransmissionError as Aria2Error
-from .transmission import is_metadata_file
-
-__all__ = ["Aria2Client", "Aria2Error", "is_metadata_file"]
-
